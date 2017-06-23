bf1342e9ef1b12e1a31c5452f944216286bdbb91
Cid Lopes <alannettto@gmail.com>
2017-06-23 16:36:53 -0300

    Updates aliases and includes oh-my-zsh

--- resources/after.sh
+++ resources/after.sh
@@ -3,3 +3,9 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+sudo apt-get install zsh -yqq
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="fino"/g'  ~/.zshrc
+echo '\n' >> ~/.zshrc
+cat ~/.bash_aliases >> ~/.zshrc
+echo 'exec zsh' >> ~/.bashrc
