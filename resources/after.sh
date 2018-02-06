795dca225b0e6ff3ab1d3aec05d14c9394a1a9d5
Alcidemar Lope <alannettto@gmail.com>
2018-02-06 14:18:21 +0200

    Set PHP version to 7.1 and add some php extensions for Magento Development

--- resources/after.sh
+++ resources/after.sh
@@ -10,3 +10,10 @@ sed -i 's/plugins=(git)/plugins=(git laravel5)/g'  ~/.zshrc
 echo '\n' >> ~/.zshrc
 cat ~/.bash_aliases >> ~/.zshrc
 echo 'exec zsh' >> ~/.bashrc
+sudo apt-get install mcrypt php7.1-mcrypt  -yqq
+sudo apt-get install php7.1-intl -yqq
+sudo apt-get install php7.1-xsl -yqq
+sudo a2dismod php7.2
+sudo a2enmod php7.1
+sudo service apache2 restart
+sudo update-alternatives --set php /usr/bin/php7.1
